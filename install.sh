db2cd632d7fac34b20f3901988115dd37c40a934
Dian Ananda <dionananda77@gmail.com>
2022-02-18 11:28:55 +0700

    create script installer

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+warnnvim() {
+  echo "Neovim not found, please install it"
+  exit
+}
+
+warnnode() {
+  echo "Node not found, please install it"
+  exit
+}
+
+warngit() {
+  echo "Git not found, please install it"
+  exit
+}
+
+changedirname() {
+  mv $HOME/.config/nvim $HOME/.config/nvim_old
+  echo "Your existing nvim config has been moving to nvim_backup"
+  clonerepo
+}
+
+clonerepo() {
+  echo "Cloning repo to your nvim directory..."
+  git clone https://github.com/dionannd/nfs ~/.config/nvim
+  nvim +PackerSync
+}
+
+which nvim >/dev/null && echo "Neovim is installed" || warnnvim
+which node >/dev/null && echo "Node is installed" || warnnode
+which git >/dev/null && echo "Git is installed" || warngit
+
+[ -d "$HOME/.config/nvim" ] && changedirname || clonerepo
