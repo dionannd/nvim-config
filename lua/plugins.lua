623683978ccdac4bc4815e50f85f35e133c92302
dionannd <dionananda77@gmail.com>
2022-08-09 22:32:19 +0700

    chore(*): update nvim config

---
+++ lua/plugins.lua
@@ -0,0 +1,20 @@
+local status, packer = pcall(require, 'packer')
+if (not status) then
+  print('packer not installed')
+  return
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+packer.startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use {
+    'svrana/neosolarized.nvim',
+    requires = { 'tjdevries/colorbuddy.nvim' }
+  }
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  } -- Statusline
+  use 'neovim/nvim-lspconfig' -- LSP
+end)
