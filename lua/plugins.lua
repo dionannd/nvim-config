1374a4e558b07672b3f86a489696da3df28bd422
dionannd <dionananda77@gmail.com>
2022-08-10 04:33:49 +0700

    feat: add comment config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -12,39 +12,43 @@ packer.startup(function(use)
     'svrana/neosolarized.nvim',
     requires = { 'tjdevries/colorbuddy.nvim' }
   }
+  use 'nvim-lua/plenary.nvim' -- Common utilities
   use 'kyazdani42/nvim-web-devicons' -- File icons
-  use 'glepnir/lspsaga.nvim'
-  use 'L3MON4D3/LuaSnip' -- Snipet
   use 'hoob3rt/lualine.nvim' -- Statusline
-  use 'onsails/lspkind-nvim' -- vscode-like pictograms
-  use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
-  use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built-in LSP
-  use 'hrsh7th/nvim-cmp' -- Completion
+  use 'akinsho/nvim-bufferline.lua'
+
   use 'neovim/nvim-lspconfig' -- LSP
+  use 'glepnir/lspsaga.nvim'
   use {
     'nvim-treesitter/nvim-treesitter',
     run = ':TSUpdate'
   }
+  use 'L3MON4D3/LuaSnip' -- Snipet
+  use 'onsails/lspkind-nvim' -- vscode-like pictograms
+  use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
+  use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built-in LSP
+  use 'hrsh7th/nvim-cmp' -- Completion
   use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server
-  use 'MunifTanjim/prettier.nvim' -- Prettier plugin for Neovim's built-in LSP client
   use 'williamboman/mason.nvim'
   use 'williamboman/mason-lspconfig.nvim'
   
-  use 'windwp/nvim-autopairs'
   use 'windwp/nvim-ts-autotag'
+  use 'windwp/nvim-autopairs'
+  use 'MunifTanjim/prettier.nvim' -- Prettier plugin for Neovim's built-in LSP client
+  use 'norcalli/nvim-colorizer.lua'
 
-  use 'nvim-lua/plenary.nvim' -- Common utilities
   use 'nvim-telescope/telescope.nvim'
   use 'nvim-telescope/telescope-file-browser.nvim'
 
-  use 'akinsho/nvim-bufferline.lua'
-  use 'norcalli/nvim-colorizer.lua'
   use 'folke/zen-mode.nvim' 
   use({
     'iamcco/markdown-preview.nvim',
     run = function() vim.fn['mkdp#util#install']() end,
   })
+  use 'numToStr/Comment.nvim'
 
   use 'lewis6991/gitsigns.nvim'
   use 'dinhhuy258/git.nvim'
+
+  use 'folke/tokyonight.nvim'
 end)
