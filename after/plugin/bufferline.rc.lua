d756eb1f27f45664716a8c77b7719caf98213af5
dionannd <dionananda77@gmail.com>
2022-08-18 04:42:57 +0700

    fix(bufferline): wrong custom area place config

--- after/plugin/bufferline.rc.lua
+++ after/plugin/bufferline.rc.lua
@@ -11,6 +11,33 @@ bufferline.setup {
     color_icons = true,
     indicator_icon = "▎",
     modified_icon = "●",
+    custom_areas = {
+      right = function()
+        local result = {}
+        local seve = vim.diagnostic.severity
+        local error = #vim.diagnostic.get(0, { severity = seve.ERROR })
+        local warning = #vim.diagnostic.get(0, { severity = seve.WARN })
+        local info = #vim.diagnostic.get(0, { severity = seve.INFO })
+        local hint = #vim.diagnostic.get(0, { severity = seve.HINT })
+
+        if error ~= 0 then
+          table.insert(result, { text = "  " .. error, fg = "#EC5241" })
+        end
+
+        if warning ~= 0 then
+          table.insert(result, { text = "  " .. warning, fg = "#EFB839" })
+        end
+
+        if hint ~= 0 then
+          table.insert(result, { text = "  " .. hint, fg = "#A3BA5E" })
+        end
+
+        if info ~= 0 then
+          table.insert(result, { text = "  " .. info, fg = "#7EA9A7" })
+        end
+        return result
+      end,
+    }
   },
   highlights = {
     fill = {
@@ -32,35 +59,7 @@ bufferline.setup {
       bold = true,
     },
   },
-  custom_areas = {
-    right = function()
-      local result = {}
-      local seve = vim.diagnostic.severity
-      local error = #vim.diagnostic.get(0, {severity = seve.ERROR})
-      local warning = #vim.diagnostic.get(0, {severity = seve.WARN})
-      local info = #vim.diagnostic.get(0, {severity = seve.INFO})
-      local hint = #vim.diagnostic.get(0, {severity = seve.HINT})
-
-      if error ~= 0 then
-        table.insert(result, {text = "  " .. error, fg = "#EC5241"})
-      end
-
-      if warning ~= 0 then
-        table.insert(result, {text = "  " .. warning, fg = "#EFB839"})
-      end
-
-      if hint ~= 0 then
-        table.insert(result, {text = "  " .. hint, fg = "#A3BA5E"})
-      end
-
-      if info ~= 0 then
-        table.insert(result, {text = "  " .. info, fg = "#7EA9A7"})
-      end
-      return result
-    end,
-  }
 }
 
 vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', {})
 vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', {})
-
