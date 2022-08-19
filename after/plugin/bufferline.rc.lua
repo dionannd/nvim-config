4cb0787e33050992e9fe38ed1b0db525c49528f3
dionannd <dionananda77@gmail.com>
2022-08-19 18:24:57 +0700

    fix(lsp): fix lspconfig format on save

--- after/plugin/bufferline.rc.lua
+++ after/plugin/bufferline.rc.lua
@@ -5,41 +5,13 @@ bufferline.setup {
   options = {
     mode = "tabs",
     numbers = 'none',
-    close_command = "bdelete! %d",
-    right_mouse_command = "bdelete! %d",
-    show_buffer_close_icons = false,
+    separator_style = 'thin',
     always_show_bufferline = false,
-    separator_style = 'slant',
-    color_icons = true,
+    show_buffer_close_icons = false,
+    --[[ show_close_icon = false,
+    color_icons = true, ]]
     indicator_icon = "▎",
     modified_icon = "●",
-    custom_areas = {
-      right = function()
-        local result = {}
-        local seve = vim.diagnostic.severity
-        local error = #vim.diagnostic.get(0, { severity = seve.ERROR })
-        local warning = #vim.diagnostic.get(0, { severity = seve.WARN })
-        local info = #vim.diagnostic.get(0, { severity = seve.INFO })
-        local hint = #vim.diagnostic.get(0, { severity = seve.HINT })
-
-        if error ~= 0 then
-          table.insert(result, { text = "  " .. error, fg = "#EC5241" })
-        end
-
-        if warning ~= 0 then
-          table.insert(result, { text = "  " .. warning, fg = "#EFB839" })
-        end
-
-        if hint ~= 0 then
-          table.insert(result, { text = "  " .. hint, fg = "#A3BA5E" })
-        end
-
-        if info ~= 0 then
-          table.insert(result, { text = "  " .. info, fg = "#7EA9A7" })
-        end
-        return result
-      end,
-    }
   },
   highlights = {
     fill = {
